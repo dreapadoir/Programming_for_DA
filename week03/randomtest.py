a2e1297f67976dc1d9b5084ea2144fdd2345e8dc
David Higgins <g00411302@gmit.ie>
2022-10-22 19:41:01 +0100

    added binomial code

--- week03/randomtest.py
+++ week03/randomtest.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 list = list(range(10))
 
 print(list)
@@ -8,3 +9,15 @@ liste = np.random.default_rng()
 print(liste.random())
 
 print(liste.integers(19))
+
+liste = np.random.default_rng(seed = 42)
+
+print(liste.random())
+
+
+rng = np.random.default_rng()
+n, p = 10, .5  # number of trials, probability of each trial
+s = rng.binomial(n, p, 10000000)
+
+plt.hist(s)
+plt.show()
